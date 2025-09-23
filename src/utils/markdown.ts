049b6230f928780ab027d0cd952e758ab40665d4
Sajal Choudhary <sajal.choudhary@gmail.com>
2025-09-23 17:36:12 +0300

    Refactor markdown parsing

---
+++ src/utils/markdown.ts
@@ -0,0 +1,10 @@
+import { marked } from 'marked';
+
+marked.setOptions({
+  gfm: true,
+  breaks: true
+});
+
+export function parseMarkdown(content: string): string {
+  return marked.parse(content) as string;
+}
